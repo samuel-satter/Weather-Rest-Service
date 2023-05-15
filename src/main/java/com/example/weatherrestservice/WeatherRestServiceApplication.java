8c1a965a723f1ce89163ed924504c13c0cb7c5c5
samuel-satter <97049361+samuel-satter@users.noreply.github.com>
2023-05-15 09:35:24 +0200

    Initial commit

---
+++ src/main/java/com/example/weatherrestservice/WeatherRestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.weatherrestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherRestServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/weatherrestservice/WeatherRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.weatherrestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherRestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
